04cc6aa00ad7942ecbbd5f927a2f13ccc9a60ada
Vincent Guidry <vguidry@astralbrands.com>
2013-08-28 15:13:27 -0400

    .

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -60,7 +60,7 @@ FacebookQuizzes::Application.configure do
   # Precompile additional assets.
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
   # config.assets.precompile += %w( search.js )
-  config.assets.precompile += %w( *.js *.scss *.coffee *.css *.sass )
+  config.assets.precompile += %w( *.js *.coffee *.css *.sass )
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false

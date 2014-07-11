c26752e54105bed3fe653479e70c956030692c77
Vincent Guidry <vguidry@gmail.com>
2014-07-11 13:58:04 -0400

    removed paperclip from environment.rb

--- config/environment.rb
+++ config/environment.rb
@@ -3,5 +3,3 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 FacebookQuizzes::Application.initialize!
-
-Paperclip.options[:command_path] = ENV['IMAGEMAGICK_PATH']
